720eaf6a49cf3226985521fb86f950a58eda28b1
Rifat Arefin <arefinrifat786@gmail.com>
2026-03-18 23:45:11 +0600

    class-02-function based view for product details endpoint

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.core.views import product_list
+from apps.core.views import product_list, product_detail
 
 urlpatterns = [
     path('products/', product_list, name='product-list'),
+    path('products/<int:pk>', product_detail, name='product-detail'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -10,3 +10,13 @@ def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+# Product detail view (function-based view)
+@api_view(['GET'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
