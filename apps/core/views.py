9f1fc2ea37049e2c6a7013f1c916603399aa2056
Rifat Arefin <arefinrifat786@gmail.com>
2026-03-18 23:53:00 +0600

    class-02-function based view for product list and product details with django shortcut

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -20,3 +21,24 @@ def product_detail(request, pk):
         return Response(serializer.data, status=status.HTTP_200_OK)
     except Product.DoesNotExist:
         return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+# Product lists view (function-based view with shortcut)
+@api_view(['GET'])
+def product_list_shortcut(request):
+    products = get_list_or_404(Product)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+# Product detail view (function-based view with shortcut)
+@api_view(['GET'])
+def product_detail_shortcut(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+# Product detail view (function-based view with shortcut)
+@api_view(['GET'])
+def product_detail_shortcut(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data, status=status.HTTP_200_OK)
