d9db7f04aab5c73958703fb38ba20ab894080846
Rifat Arefin <arefinrifat786@gmail.com>
2026-03-18 21:09:04 +0600

    class-01-create custom user model

--- learn_DRF/settings.py
+++ learn_DRF/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.core',
-    'apps.accoount'
+    'apps.account'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'account.User'
