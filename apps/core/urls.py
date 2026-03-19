85aa04a83dc935d5f5e0753eb1ad5dc618d5d81b
Rifat Arefin <arefinrifat786@gmail.com>
2026-03-19 18:02:54 +0600

    class-0-add APIView class based view for product info

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,7 +4,7 @@ from apps.core import views
 urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='product-list'),
     path('products/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
-    path('product-info/', views.product_info, name='product-info'),
+    path('product-info/', views.ProoductInfoApiView.as_view(), name='product-info'),
     path('all-orders/', views.AllOrderListView.as_view(), name='all-order-list'),
     path('my-orders/', views.MyOrderListView.as_view(), name='my-order-list'),
 ]
